39231000d22e552996ea11ee37750cdc92d66a3d
romiwijayanti <romiwijayanti0@gmail.com>
2019-04-12 14:34:24 +0700

    blog

--- blog/app/Http/Controllers/produkController.php
+++ blog/app/Http/Controllers/produkController.php
@@ -1,17 +1,16 @@
 <?php
-
-namespace App\Http\Controllers; 
-use Illuminate\Http\Request; 
-class produkController extends Controller 
-{ 
-	public function index() 
-	{ 
-		$produk = 'Aqua 400ML'; 
-		return view('produk/index',compact('produk')); 
-	} 
-	public function show() 
-	{ 
-		$produk = ['Aqua 115 ML','Minuman Bersoda','Buku Sejarah','Mouse','CPU']; 
-		return view('produk/show',compact('produk')); 
-	} 
-} 
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class produkController extends Controller
+{
+public function index()
+{
+$produk = ['Meja','Kursi','Buku','Lampu'];
+	return view('produk.index', compact('produk'));
+}
+public function show()
+{
+$produk = ['Aqua 115 ML','Minuman Bersoda','Buku Sejarah','Mouse','CPU'];
+return view('produk/show',compact('produk'));
+}
+}
--- blog/resources/views/pelanggan/index.blade.php
+++ blog/resources/views/pelanggan/index.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Pelanggan</title>
 </head>
 <body>
-	<h1>Data Produk</h1>
+	<h1>Data Pelanggan</h1>
 	<table border="1">
 		<thead>
+			<tr>
 				<th>No</th>
-			<th>Nama Produk</th>
+			<th>Nama</th>
+			</tr>
 		</thead>
 	<tbody>
-		@foreach ($produk as $i => $v)
+		@foreach ($pelanggan as $i => $v)
 			<tr>
 				<td>{{ $i+1 }}</td>
 				<td>{{ $v }}</td>
